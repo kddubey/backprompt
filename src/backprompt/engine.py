ae422d385384f7c374b07bf06e0241817b739f09
Kush Dubey <kushdubey63@gmail.com>
2023-04-17 23:28:58 -0700

    Dont cat past_key_values again

--- src/backprompt/engine.py
+++ src/backprompt/engine.py
@@ -19,43 +19,6 @@ def _cat_encodings(
     return BatchEncoding({"input_ids": input_ids, "attention_mask": attention_mask})
 
 
-def _past_key_values_as_tensor(
-    past_key_values: tuple[tuple[torch.Tensor, torch.Tensor]]
-) -> torch.Tensor:
-    ## For layer i, past_key_values[i] is a tuple (key, value),
-    ## each w/ shape: (batch size=1,
-    ##                 number of attention heads=12 for gpt2,
-    ##                 # input tokens,
-    ##                 key/value hidden dimension=64 for gpt2)
-    return torch.stack([torch.stack(block) for block in past_key_values], dim=0)
-
-
-def _cat_out(
-    out_past: CausalLMOutputWithCrossAttentions, out: CausalLMOutputWithCrossAttentions
-) -> CausalLMOutputWithCrossAttentions:
-    """
-    Horizontally concatenate `out_past.past_key_values` and `out.past_key_values` along
-    the token dimension.
-    """
-    past_key_values = _past_key_values_as_tensor(out_past.past_key_values)
-    key_values = _past_key_values_as_tensor(out.past_key_values)
-    ## The tuples are now tensors, each w/ shape:
-    ## (# layers=12 for gpt2,
-    ##  2 (for key and value),
-    ##  batch size=1,
-    ##  number of attention heads=12 for gpt2,
-    ##  # input tokens,
-    ##  key/value hidden dimension=64 for gpt2)
-    ## Concatenate the pair along the token dimension, and reformat it back to the tuple
-    out.past_key_values = tuple(
-        [
-            (layer[0], layer[1])
-            for layer in torch.cat((past_key_values, key_values), dim=4)
-        ]
-    )
-    return out
-
-
 def call_model_given_past(
     model: GPT2Model,
     tokenizer: GPT2Tokenizer,
@@ -95,10 +58,7 @@ def call_model_given_past(
             past_key_values=out_past.past_key_values,
             position_ids=position_ids,
         )
-    ## Annoying part: concatenate (encoding_past, encoding) and (out_past, out) for any
-    ## future text
     encoding = _cat_encodings(encoding_past, encoding)
-    out = _cat_out(out_past, out)
     return encoding, out
 
 
@@ -115,7 +75,7 @@ class Text:
     ):
         self.string = string
         self.model_and_tokenizer = model_and_tokenizer
-        self.model_repr = None
+        self.model_repr: tuple[BatchEncoding, CausalLMOutputWithCrossAttentions] = None
         ## internal variables used for autograd graph construction
         self._forward = lambda: None  ## TODO: replace w/ partial'd model call?
         self._prev = _prev
@@ -175,14 +135,3 @@ class Text:
             string_shown = self.string[:start] + joiner + self.string[-end:]
         string_shown = repr(string_shown)  ## handle single and double quotes
         return f"{self.__class__.__name__}({string_shown})"
-
-
-# from transformers import AutoModelForCausalLM, AutoTokenizer
-# gpt2 = AutoModelForCausalLM.from_pretrained('gpt2')
-# tokenizer = AutoTokenizer.from_pretrained('gpt2')
-# tokenizer.pad_token = tokenizer.eos_token
-# model_and_tokenizer = (gpt2, tokenizer)
-# from engine import Text
-# context = Text('context', model_and_tokenizer)
-# request1 = Text(' r1', model_and_tokenizer)
-# request2 = Text(' r2', model_and_tokenizer)
